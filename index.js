8078c886f2b25bb1c343f75970441d8578f3e2e1
Julien Sanselme <julien.sanselme@gmail.com>
2015-10-14 16:03:44 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var Discord = require("discord.js");
+
+// Get the email and password
+var AuthDetails = require("./auth.json");
+
+var bot = new Discord.Client();
+var adjs = ["ELECTRICAL", "RADIOACTIVE", "SPACE", "LAZER SHOOTING"];
+var colors = ["RED", "BLUE", "GOLDEN", "GREEN"];
+var nouns = ["PONEY", "SNAKE", "GOAT", "COW"];
+
+bot.on("ready", function () {
+	bot.setPlayingGame(234);
+	console.log("Ready to begin ! Serving in " + bot.channels.length + " channels");
+});
+
+bot.on("disconnected", function () {
+	process.exit(1); //exit node.js with an error
+});
+
+bot.on("message", function (msg) {
+	var msg_args = msg.content.split(" ");
+
+	if (msg_args[0] === "ping") {
+		bot.sendMessage(msg.channel, "Pong !");
+	} else if (msg_args[0] === "bot" && msg_args[1] === undefined) {
+		bot.sendMessage(msg.channel, "Commandes disponibles : \n - /osfrog \n - /roll [nbDice] [rollMax]");
+	} else if (msg_args[0].toLowerCase() === "/osfrog") {
+		bot.sendMessage(msg.channel, "LE BALANCED " + randElem(adjs) + " " + randElem(colors) + " " + randElem(nouns));
+	} else if (msg_args[0].toLowerCase() === "/roll") {
+		if (parseInt(msg_args[1],10) && parseInt(msg_args[2],10) && msg_args[3] === undefined) {
+			bot.sendMessage(msg.channel, "Your roll : " + rollTheDice(parseInt(msg_args[1], 10), parseInt(msg_args[2], 10)));
+		} else {
+			bot.sendMessage(msg.channel, "Syntaxe incorrecte : /roll [nbDice] [rollMax]");
+		}
+	}
+});
+
+var randElem = function(tab) {
+	var max = tab.length - 1;
+	var rand = Math.floor(Math.random()*max);
+	console.log(max, rand);
+	return tab[rand];
+}
+
+var rollTheDice = function(nbRoll, maxRoll) {
+	if (maxRoll < 1) maxRoll = 1;
+	if (maxRoll > 100) maxRoll = 100;
+	if (nbRoll < 1) nbRoll = 1;
+	if (nbRoll > 10) nbRoll = 10;
+
+	var rolls = '# (1 - ' + maxRoll  + ') x '  + nbRoll + ' -> ';	
+	for (var ii = 0; ii < nbRoll; ii++) {
+		rollValue = Math.floor(Math.random()*maxRoll) + 1;
+		rolls += '[' + rollValue + '] ';
+	}
+	return rolls + '#';
+}
+
+bot.login(AuthDetails.email, AuthDetails.password);
